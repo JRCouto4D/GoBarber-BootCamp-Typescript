8fa3eaa858e9a69f636da45a3d07a69d6ae94060
unknown <jeffynho32@gmail.com>
2021-01-06 15:00:19 -0300

    feat(migrations): change type of id column of appointmetns and users tables

--- backend/src/database/migrations/1609948613023-users.ts
+++ backend/src/database/migrations/1609948613023-users.ts
@@ -8,7 +8,7 @@ export default class users1609948613023 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
