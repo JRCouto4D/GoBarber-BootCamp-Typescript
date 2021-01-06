bcbfbf8cf6fde478d3a044c0680e071497b448ed
unknown <jeffynho32@gmail.com>
2021-01-06 16:42:38 -0300

    feat: defining feature for creating user with encrypted password

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import AppointmentRouter from './appointments.routes';
+import UserRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointment', AppointmentRouter);
+routes.use('/users', UserRouter);
 
 export default routes;
