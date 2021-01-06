a905b0be24564f4b5e6f9c4a3bd5e89dc7d54f66
unknown <jeffynho32@gmail.com>
2021-01-06 13:51:16 -0300

    feat(appointments): adjustment in the appointments table

--- backend/src/database/migrations/1609941509908-CreateAppointments.ts
+++ backend/src/database/migrations/1609941509908-CreateAppointments.ts
@@ -24,6 +24,16 @@ export default class CreateAppointments1609941509908
             type: 'timestamp with time zone',
             isNullable: false,
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
