f3bc941f13249a6ca4a6fc7825e07be36f72c69c
unknown <jeffynho32@gmail.com>
2021-01-06 12:20:18 -0300

    feat: including default parameter in the id column of the appointments table

--- backend/src/database/migrations/1609941509908-CreateAppointments.ts
+++ backend/src/database/migrations/1609941509908-CreateAppointments.ts
@@ -5,13 +5,14 @@ export default class CreateAppointments1609941509908
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointmens',
+        name: 'appointments',
         columns: [
           {
             name: 'id',
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
